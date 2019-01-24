7f05d064decc7bcf7ec9718a25c0352d9f6740b7
itning <itning@itning.top>
2019-01-24 11:01:56 +0800

    fix bug

--- src/components/teacher/WorkDetails.vue
+++ src/components/teacher/WorkDetails.vue
@@ -4,10 +4,9 @@
       <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
     </md-content>
     <md-empty-state v-if="show_empty"
-                    md-icon="work_off"
-                    md-label="没有作业"
-                    md-description="该群组没有已经创建的作业，点击下方按钮来创建第一个作业">
-      <md-button class="md-primary md-raised" @click="addBtn">创建作业</md-button>
+                    md-icon="work_outline"
+                    md-label="没有作业信息"
+                    md-description="目前还没有同学加入这个群组，请先邀请同学加入群组吧">
     </md-empty-state>
     <md-table v-if="have_work" v-show="init_finish" v-model="searched" md-sort="student.no" md-sort-order="asc" md-card
               md-fixed-header>
